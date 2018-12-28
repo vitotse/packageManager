9b126f29c367c912d06529975ada7f88d428a9b3
vitotse88 <vitotse88@km-it.cn>
2018-12-28 12:22:33 +0800

    update 0.0.2

---
+++ PackageManage/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  UserModel.swift
+//  PackageManage
+//
+//  Created by vitotse88 on 2018/12/28.
+//  Copyright © 2018 vitotse88. All rights reserved.
+//
+
+import Foundation
+
+class UserModel {
+    var name = "VT"
+    var age = 20
+}
